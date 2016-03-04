92740ad0eae2580365ce6d66fd195944e24c8b12
Juan Pedro Torres Muñoz <juanp.95.torres@gmail.com>
2016-03-04 01:35:30 +0100

    Primeras modificaciones, peticion de datos y comprobación de contraseña.

--- Practica2/src/Principal/Principal.java
+++ Practica2/src/Principal/Principal.java
@@ -1,6 +1,8 @@
 package Principal;
 
 import java.io.Console;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 /**
  * Created by JuanP on 03/03/2016
@@ -10,7 +12,7 @@ public class Principal {
         /**
          *
          * Bloque para leer por consola sin mostrar lo que escribimos, puede ser util más adelante.
-         *
+         * Para ejecutar por consola -> desde fuera del paquete  "java nombrepaquete.Principal"
          *
          *
          */
@@ -20,5 +22,20 @@ public class Principal {
         System.out.println(pass);*/
 
         //TODO implementar primeros pasos con Scanner y sin cabecera.
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduzca contraseña : ");
+        String pass1 = in.next();
+        System.out.println("Repita contraseña : ");
+        String pass2 = in.next();
+
+        if (pass1.equals(pass2)){
+            System.out.println("Cifrando...");
+            //TODO implementar el cifrado del fichero en args[] ó pedido el nombre
+        }else {
+            System.out.println("Las contraseñas no coinciden.");
+        }
+
+
     }
 }
