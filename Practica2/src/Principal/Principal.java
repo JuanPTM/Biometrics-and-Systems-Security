22c70b9ab3ddccfedb26ea02244444e07d46b00b
Juan Pedro Torres Muñoz <juanp.95.torres@gmail.com>
2016-03-04 01:25:36 +0100

    Organización de carpetas, creación proyecto estado base.

---
+++ Practica2/src/Principal/Principal.java
@@ -0,0 +1,24 @@
+package Principal;
+
+import java.io.Console;
+
+/**
+ * Created by JuanP on 03/03/2016
+ */
+public class Principal {
+    public static void main(String[] args) {
+        /**
+         *
+         * Bloque para leer por consola sin mostrar lo que escribimos, puede ser util más adelante.
+         *
+         *
+         *
+         */
+
+        /*Console cons = System.console();
+        char[] pass = cons.readPassword("%s","Introduzca contraseña : ");
+        System.out.println(pass);*/
+
+        //TODO implementar primeros pasos con Scanner y sin cabecera.
+    }
+}
